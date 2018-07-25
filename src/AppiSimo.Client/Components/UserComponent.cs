c988f33256314b9824e5d338ba6ce385593664fc
Alberto Viezzi <aviezzi@gmail.com>
2018-07-26 00:53:38 +0200

    Added parameterized route component

---
+++ src/AppiSimo.Client/Components/UserComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace AppiSimo.Client.Components
+{
+    using Microsoft.AspNetCore.Blazor.Components;
+    
+    public class UserComponent : BlazorComponent
+    {
+        [Parameter]
+        protected string Id { get; set; } = "fantastic";   
+    }
+}
