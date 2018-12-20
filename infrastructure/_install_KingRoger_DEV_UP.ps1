7fe9ffccf8f0dbbc466230aaa3966507b1d95277
Alberto Viezzi <aviezzi@gmail.com>
2018-12-20 13:47:56 +0100

    Migrated postgres DB script to ps1.

---
+++ infrastructure/_install_KingRoger_DEV_UP.ps1
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# infrastructure
+
+docker-compose -f postgres/docker-compose.yml up -d
--- infrastructure/_install_KingRoger_DEV_UP.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# infrastructure
-
-sudo docker-compose -f postgres/docker-compose.yml up -d
