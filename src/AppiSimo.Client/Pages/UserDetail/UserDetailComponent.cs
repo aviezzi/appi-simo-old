c50257dc0fed693261c28d53bfd6da8ade78f031
Alberto Viezzi <aviezzi@gmail.com>
2019-01-03 18:22:09 +0100

    Refactoring

--- src/AppiSimo.Client/Pages/UserDetail/UserDetailComponent.cs
+++ src/AppiSimo.Client/Pages/UserDetail/UserDetailComponent.cs
@@ -1,10 +1,8 @@
-namespace AppiSimo.Client.Pages.UserDetail
+﻿namespace AppiSimo.Client.Pages.UserDetail
 {
     using System;
-    using System.Linq;
     using System.Threading.Tasks;
     using AppiSimo.Shared.Model;
-    using Clients;
     using Microsoft.AspNetCore.Blazor.Components;
     using Microsoft.AspNetCore.Blazor.Services;
     using Shared.Pages.Abstract;
@@ -15,37 +13,27 @@ namespace AppiSimo.Client.Pages.UserDetail
         string Id { get; set; }
 
         [Inject]
-        AppiSimoClient<User> Client { get; set; }
-
-        [Inject]
         IUriHelper UriHelper { get; set; }
 
         protected User User { get; private set; } = new User();
 
-        readonly EndPoint<User> _endPoint;
-
-        protected UserDetailComponent()
-        {
-            _endPoint = Client.GetEndPoint();
-        }
-        
         protected override async Task OnInitAsync()
         {
             if ((Id != null) & Guid.TryParse(Id, out var id))
             {
-                User = (await _endPoint.Entities.Where(u => u.Id == id).ToListAsync(Client.Client)).Value.FirstOrDefault();
+                User = await EndPoint.Entity(id);
             }
         }
 
         protected async Task Submit()
         {
-            await _endPoint.Save(User);
+            await EndPoint.Save(User);
             GoToHome();
         }
 
         protected async Task Delete()
         {
-            await _endPoint.Remove(User.Id);
+            await EndPoint.Delete(User.Id);
             GoToHome();
         }
 
