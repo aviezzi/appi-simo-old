6decda095839e1f33a961d480576fc897e544498
Alberto Viezzi <aviezzi@gmail.com>
2019-04-10 09:35:45 +0200

    Change connections string and docker-compose

---
+++ infrastructure/postgres/db_data_init.sql
@@ -0,0 +1,7 @@
+INSERT INTO "Lights" ("Id", "LightType", "Price") values 
+(uuid_generate_v4(), 'Led', 1.00),
+(uuid_generate_v4(), 'Standard', 2.00);
+
+INSERT INTO "Heats" ("Id", "HeatType", "Price") values
+(uuid_generate_v4(), 'Gas', 3.00),
+(uuid_generate_v4(), 'Heat Pump', 2.00);
