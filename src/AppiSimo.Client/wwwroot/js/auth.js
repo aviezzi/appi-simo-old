88ade0fe68b2c7ca3fb0bf1a6eb50dc635ba1c1f
Alberto Viezzi <aviezzi@gmail.com>
2019-05-03 23:52:58 +0200

    Move manifest from fucking cognito to local settings

--- src/AppiSimo.Client/wwwroot/js/auth.js
+++ src/AppiSimo.Client/wwwroot/js/auth.js
@@ -7,6 +7,20 @@
             authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_jUNe13QJ4',
             client_id: 'ld3qolihulq7pg0meehtfv20e',
 
+            metadata: {
+                "authorization_endpoint":"https://appi-simo.auth.eu-central-1.amazoncognito.com/oauth2/authorize",
+                "id_token_signing_alg_values_supported":["RS256"],
+                "issuer":"https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_jUNe13QJ4",
+                "jwks_uri":"https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_jUNe13QJ4/.well-known/jwks.json",
+                "response_types_supported":["code","token","token id_token"],
+                "scopes_supported":["openid","email","phone","profile"],
+                "subject_types_supported":["public"],
+                "token_endpoint":"https://appi-simo.auth.eu-central-1.amazoncognito.com/oauth2/token",
+                "token_endpoint_auth_methods_supported":["client_secret_basic","client_secret_post"],
+                "userinfo_endpoint":"https://appi-simo.auth.eu-central-1.amazoncognito.com/oauth2/userInfo",
+                end_session_endpoint: 'https://appi-simo.auth.eu-central-1.amazoncognito.com/logout?logout_uri=https://localhost:5003&client_id=ld3qolihulq7pg0meehtfv20e&response_type=token' 
+            },
+            
             automaticSilentRenew: true,
             redirect_uri: 'https://localhost:5003/signed-in',
             post_logout_redirect_uri: '/',
